e8e879841d636e2e0c737f7e6950f53b400106bd
Jack Cushman <jcushman@law.harvard.edu>
2021-05-25 13:55:21 -0400

    Add federal administrative and executive materials

--- tests.py
+++ tests.py
@@ -155,7 +155,7 @@ class BaseTestCase(TestCase):
 
     def check_ascii(self, obj):
         """Check that all strings in obj match a list of expected ascii characters."""
-        allowed_chars = r"[ 0-9a-zA-Z.,\-'&(){}\[\]\\$§_?<>+:/]"
+        allowed_chars = r"[ 0-9a-zA-Z.,\-'&(){}\[\]\\$§_?<>+*|:/]"
         for s in emit_strings(obj):
             remaining_chars = re.sub(allowed_chars, "", s)
             self.assertFalse(
