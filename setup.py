3f3eb951921211f1cdcdafd006c93671a6b8d62e
William Palin <flooie@users.noreply.github.com>
2021-09-09 17:19:24 -0400

    version bump v3.2.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup, find_packages
 
 
-VERSION = "3.2.2"
+VERSION = "3.2.3"
 AUTHOR = "Mike Lissner"
 EMAIL = "mike@free.law"
 HERE = os.path.abspath(os.path.dirname(__file__))
