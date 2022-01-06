79c3abf0639e7215cf17a8229af8a54f865f6350
William Palin <bill@free.law>
2022-01-06 12:50:45 -0500

    version bump v3.2.9

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 from setuptools import find_packages, setup
 
-VERSION = "3.2.8"
+VERSION = "3.2.9"
 AUTHOR = "Mike Lissner"
 EMAIL = "mike@free.law"
 HERE = os.path.abspath(os.path.dirname(__file__))
